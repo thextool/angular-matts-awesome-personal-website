6ea95f820d92df764980d2dfc92677967dad3083
thextool <matthew.riach@gmail.com>
2018-10-31 19:40:02 -0600

    :zap: added a header with some text for the about me section with a placeholder imagae

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule
